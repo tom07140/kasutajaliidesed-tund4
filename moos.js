28485b516e755f23a042ad4ad68ba44754069875
Toomas Lõõnik <toomloo@greeny.cs.tlu.ee>
2016-02-22 10:48:20 +0200

    started working on search field

--- moos.js
+++ moos.js
@@ -20,6 +20,9 @@
     this.currentRoute = null;
     console.log(this);
 
+    //hakkan hoidma kõiki purke
+    this.jars=[];
+
     //kui tahan moosipurgile referenci siis kasuton THIS = MOOSIPURGI RAKENDUS ISE
     this.init();
 
@@ -69,20 +72,45 @@
         this.routeChange();
       }
 
+      //saan kätte purgid localStorage kui on
+      if(localStorage.jars){
+        //võtan stringi ja teen tagasi objektideks
+        this.jars = JSON.parse(localStorage.jars);
+        console.log('laadisin localStorageist massiivi ' + this.jars.length);
+
+        //tekitan loendi htmli
+        this.jars.forEach(function(jar){
+
+          var new_jar = new Jar(jar.title, jar.ingredients);
+
+          var li = new_jar.createHtmlElement();
+          document.querySelector('.list-of-jars').appendChild(li);
+        });
+      }
+
       //kuulame hiireklikki nupul
-      this.bindMouseEvents();
+      this.bindEvents();
 
     },
-    bindMouseEvents: function(){
+    bindEvents: function(){
       document.querySelector('.add-new-jar').addEventListener('click',this.addNewClick.bind(this));
     },
     addNewClick: function(event){
       //console.log(event);
       var title = document.querySelector('.title').value;
       var ingredients = document.querySelector('.ingredients').value;
+
       //console.log(title + ' ' + ingredients);
       //1) tekitan uue Jar'i
        var new_jar = new Jar(title, ingredients);
+
+       //lisan massiivi purgi
+       this.jars.push(new_jar);
+       console.log(JSON.stringify(this.jars));
+
+       //JSON'i stringina salvestan localStorage'isse
+       localStorage.setItem('jars', JSON.stringify(this.jars));
+
        // 2) lisan selle htmli listi juurde
        var li = new_jar.createHtmlElement();
        document.querySelector('.list-of-jars').appendChild(li);
